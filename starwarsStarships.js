36ed5ed87f07600a79b5809e4c19d5a50628807c
Caio Mateus <caiomateusgames@outlook.com>
2019-05-08 10:17:15 -0300

    Adicionando as informaçoes sobre as naves.

---
+++ starwarsStarships.js
@@ -0,0 +1,63 @@
+var response;
+var selected;
+
+document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+        PegarNaves();
+
+        document.querySelector('#naves').onchange=mostrarFilmes;
+    }
+}
+
+function PegarNaves(){
+    var httpRequest = new XMLHttpRequest();
+
+    httpRequest.onreadystatechange = function(){
+        if (httpRequest.readyState ===4){
+            if (httpRequest.status === 200)
+            console.log(httpRequest.responseText);
+            response = JSON.parse(httpRequest.responseText);
+
+            var lista = document.querySelector("#naves");
+
+            lista.innerHTML = ""
+
+            response.results.forEach(function(el){
+                option = document.createElement("option");
+                option.innerHTML = el.name;
+                option.setAttribute('films', JSON.stringify(el.films))
+                lista.appendChild(option);
+
+            })
+        }
+    }
+
+    httpRequest.open('GET', 'https://swapi.co/api/starships/');
+    httpRequest.send();
+}
+
+function mostrarFilmes(ev){
+selected = ev.target;
+films = JSON.parse(selected.selectedOptions[0].getAttribute('films'));
+
+var lista = document.querySelector("#filmes")
+lista.innerHTML = ""
+films.forEach(function(url){
+    var httpRequest = new XMLHttpRequest();
+
+    httpRequest.onreadystatechange = function(){
+    if (httpRequest.readyState === 4){
+        if (httpRequest.status === 200){
+            film = JSON.parse(httpRequest.responseText);
+
+            li = document.createElement("li");
+            li.innerHTML = film.title;
+            lista.appendChild(li);
+        }
+    }
+    }
+
+    httpRequest.open('GET', url);
+    httpRequest.send();
+})
+}
